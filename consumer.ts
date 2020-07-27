4d3451b43f0c05bf0b051da0cdf1c41c43b70c9b
Akshit Garg <garg.akshit@gmail.com>
2020-07-28 01:16:47 +0530

    [Microservices - 1] Extend the consumer

--- consumer.ts
+++ consumer.ts
@@ -1,4 +1,5 @@
 import { createRemote } from "./deps.ts";
 
 const remote = createRemote("http://localhost:9876");
-console.log(await remote.add(10, 2));
+console.log("Adding 10 and 2 gives", await remote.add(10, 2));
+console.log("Subtracting 10 from 2 gives", await remote.subtract(10, 2));
