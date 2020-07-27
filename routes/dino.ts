499e4cee6e28f57da35a5cb6bef15439ffa9c6c9
Akshit Garg <garg.akshit@gmail.com>
2020-07-27 17:46:54 +0530

    [Advanced Routing] Initial Commit

--- routes/dino.ts
+++ routes/dino.ts
@@ -1,4 +1,4 @@
-import { Router } from "../deps.ts";
+import { Router, helpers } from "../deps.ts";
 
 const router = new Router({
   prefix: "/dino",
@@ -6,6 +6,24 @@ const router = new Router({
 
 router.get("/hello", (ctx) => {
   ctx.response.body = "Mr. Dino says Hello!";
+});
+
+router.get("/", (ctx) => {
+  const query = helpers.getQuery(ctx);
+
+  ctx.response.body = `Hello, I am ${query.name}. I am ${query.age} years old.`;
+});
+
+router.get("/:dino", (ctx) => {
+  ctx.response.body = `Mr. ${ctx.params.dino} says Hello!`;
+});
+
+router.post("/new", async (ctx) => {
+  const data = await ctx.request.body({
+    type: "json",
+  }).value;
+
+  ctx.response.body = `A dino with name ${data.name} created`;
 });
 
 export default router;
