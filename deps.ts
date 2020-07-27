82ce09bd64d00cd235cc845b0602c3b9576982e6
Akshit Garg <garg.akshit@gmail.com>
2020-07-28 01:00:24 +0530

    [Microservices - 1] Remove the unused dependency

--- deps.ts
+++ deps.ts
@@ -1,6 +1,3 @@
-export {
-  serve,
-  ServerRequest,
-} from "https://deno.land/std@0.62.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.62.0/http/server.ts";
 export { respondRpc } from "https://deno.land/x/gentleRpc/rpcServer.ts";
 export { createRemote } from "https://deno.land/x/gentleRpc/rpcClient.ts";
